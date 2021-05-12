2b76b5296c6b74ef73683c448572158dd5fcd58c
Massanemo <Massanemo@DESKTOP-915REQJ>
2021-05-12 15:19:34 -0300

    Codigo completo.

--- src/modelo/Calcular.java
+++ src/modelo/Calcular.java
@@ -15,6 +15,26 @@ public class Calcular extends absPropriedade
         this.seno = Math.sin(this.n1);
         this.cosseno = Math.cos(this.n1);
         this.tangente = Math.tan(this.n1);
+        if (this.n1 == Math.toRadians(90))
+        {
+            this.cosseno = 0.0;
+            this.tangente = 1.0/0;
+        }
+        else if (this.n1 == Math.toRadians(180))
+        {
+            this.seno = 0.0;
+            this.tangente = 0.0/1;
+        }
+        else if (this.n1 == Math.toRadians(270))
+        {
+            this.cosseno = 0.0;
+            this.tangente = 1.0/0;
+        }
+        else if (this.n1 == Math.toRadians(360))
+        {
+            this.seno = 0.0;
+            this.tangente = 0.0/1;
+        }
         this.toString();
     }
 }
